5bda4699d5112df22d93f65798629cf0efd911a5
zxver1000 <wlwhs5014@naver.com>
2022-12-25 23:58:56 +0900

    update firesotre

--- core/configReader.js
+++ core/configReader.js
@@ -1,104 +1,91 @@
-import InappropriateConfigKeyException from '../exception/inappropriateConfigKeyException.js';
-import OutofConfigKeyException from '../exception/outofConfigKeyException.js';
-import { readFile } from 'fs/promises';
+import InappropriateConfigKeyException from "../exception/inappropriateConfigKeyException.js";
+import OutofConfigKeyException from "../exception/outofConfigKeyException.js";
+import { readFile } from "fs/promises";
 //import generalInfo from '../configs/default.json' assert {type: "json"};
 const generalInfo = JSON.parse(
-  await readFile(
-    new URL('../configs/default.json', import.meta.url)
-  )
+  await readFile(new URL("../configs/default.json", import.meta.url))
 );
 
-import ApiReader from './apiReader.js';
-import apiType from './enum/apiType.js';
+import ApiReader from "./apiReader.js";
+import apiType from "./enum/apiType.js";
 
-
-const controlList = [
-    "cors", "restapi", "database", "jwt", "file-transfer"
-]
+const controlList = ["cors", "restapi", "database", "jwt", "file-transfer"];
 
 const properties = [
-    [
-        'default', 'origin', 'methods', 'allow-headers'
-    ],
-    [
-        'use', 'base-uri', 'count'
-    ],
-    [
-        'type', 'id', 'pw', 'host', 'port', 'scheme',"apiKey",
-        "authDomain","projectId","storageBucket","messagingSenderId","appId","measurementId"
-    ],
-    [
-        'use',
-        'generate-uri',
-        'lifetime',
-        'secret',
-        'auth-table',
-        'auth-columns',
-        'alg',
-        'columns',
-        'keys'
-    ],
-    [
-        'use',
-        'type',
-        'base-uri',
-        'read-uri',
-        'count',
-        'base-directory',
-        'table',
-        'columns'
-    ]
-]
+  ["default", "origin", "methods", "allow-headers"],
+  ["use", "base-uri", "count"],
+  ["type", "id", "pw", "host", "port", "scheme"],
+  [
+    "use",
+    "generate-uri",
+    "lifetime",
+    "secret",
+    "auth-table",
+    "auth-columns",
+    "alg",
+    "columns",
+    "keys",
+  ],
+  [
+    "use",
+    "type",
+    "base-uri",
+    "read-uri",
+    "count",
+    "base-directory",
+    "table",
+    "columns",
+  ],
+];
 
-export default class ConfigReader{
-    static instance;
+export default class ConfigReader {
+  static instance;
 
-    constructor(){
-        if(ConfigReader.instance) return ConfigReader.instance;
-        this.configInfo = new Map();
-        this.readGeneralInfo();
-        ConfigReader.instance = this;
-    }
+  constructor() {
+    if (ConfigReader.instance) return ConfigReader.instance;
+    this.configInfo = new Map();
+    this.readGeneralInfo();
+    ConfigReader.instance = this;
+  }
 
-    getConfig(){
-        return this.configInfo.get(apiType.GENERAL)
-    }
+  getConfig() {
+    return this.configInfo.get(apiType.GENERAL);
+  }
 
-    setConfigReaders(){
-        this.configInfo.set(apiType.REST, new ApiReader());
-    }
+  setConfigReaders() {
+    this.configInfo.set(apiType.REST, new ApiReader());
+  }
 
-    readGeneralInfo() {
-        this.checkControlSetting(generalInfo);
-        this.configInfo.set(apiType.GENERAL, generalInfo);
-    };
-    
-    checkControlSetting(json){
-        let i_list = [controlList];
+  readGeneralInfo() {
+    this.checkControlSetting(generalInfo);
+    this.configInfo.set(apiType.GENERAL, generalInfo);
+  }
 
-        for(let i = 0; i < i_list.length; i++){
-            let current_object = i_list[i];
-  
-            for(let index in current_object){
-                if(!json[current_object[index]]){
-                    throw new OutofConfigKeyException(
-                        `Out of Key '${current_object[index]}' for '${apiType.GENERAL}' config file.`
-                    );
-                }
+  checkControlSetting(json) {
+    let i_list = [controlList];
 
-                for(let jdex in properties[index]){
-                   
-                    if(! (json[current_object[index]])[properties[index][jdex]] ){
-                        throw new OutofConfigKeyException(
-                            `Out of Key '${properties[index][jdex]}' for '${apiType.GENERAL}->${current_object[index]};' config file.`
-                        );  
-                    }
-                }
-            }
+    for (let i = 0; i < i_list.length; i++) {
+      let current_object = i_list[i];
+
+      for (let index in current_object) {
+        if (!json[current_object[index]]) {
+          throw new OutofConfigKeyException(
+            `Out of Key '${current_object[index]}' for '${apiType.GENERAL}' config file.`
+          );
         }
-    };
 
-    printConfigs(){
-        console.log(this.configInfo);
+        for (let jdex in properties[index]) {
+          if (!json[current_object[index]][properties[index][jdex]]) {
+            throw new OutofConfigKeyException(
+              `Out of Key '${properties[index][jdex]}' for '${apiType.GENERAL}->${current_object[index]};' config file.`
+            );
+          }
+        }
+      }
     }
+  }
+
+  printConfigs() {
+    console.log(this.configInfo);
+  }
 }
